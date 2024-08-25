17989acd77ee8685e1ed7c4d6dd37cdf0c34dde9
morrisaiden <mwaimorris1997@gmail.com>
2024-08-25 21:21:05 +0300

    Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,9 +4,13 @@ Unittests for the GithubOrgClient class.
 """
 
 import unittest
+from unittest import TestCase
 from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from urllib.error import HTTPError
+from parameterized import parameterized_class
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -51,6 +55,41 @@ class TestGithubOrgClient(unittest.TestCase):
         result = client._public_repos_url
 
         self.assertEqual(result, "https://api.github.com/orgs/mock_org/repos")
+        
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)])
+    def test_has_license(self, repo, license_key, ret):
+        """Method to test GithubOrgClient.has_license function"""
+
+        client = GithubOrgClient("Test value")
+        res = client.has_license(repo, license_key)
+        self.assertEqual(ret, res)
+
+
+@parameterized_class(("org_payload", "repos_payload", "expected_repos",
+                     "apache2_repos"), TEST_PAYLOAD)
+class TestIntegrationGithubOrgClient(TestCase):
+    """Class that defines attributes to test client.GithubOrgClient class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Method to prepare test fixture"""
+
+        cls.get_patcher = patch('requests.get', side_effect=HTTPError)
+        cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Method called after test method has been called"""
+
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """Method to test GithubOrgClient.public_repos function"""
+
+        res = GithubOrgClient("Test value")
+        self.assertTrue(res)
 
 
 if __name__ == "__main__":
