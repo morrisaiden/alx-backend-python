e62494b697a3920e1ba7ff72ab88f9abd415a003
morrisaiden <mwaimorris1997@gmail.com>
2024-08-25 12:57:30 +0300

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,7 @@ and correct behavior under various conditions.
 """
 
 import unittest
+from client import GithubOrgClient
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import memoize, access_nested_map, get_json
@@ -129,6 +130,39 @@ class TestMemoize(unittest.TestCase):
             mock_method.assert_called_once()
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Tests for the GithubOrgClient class.
+
+    This class includes tests to ensure that the org method of
+    GithubOrgClient returns the correct value and behaves as expected
+    when interacting with the get_json method.
+    """
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name: str, mock_get_json: Mock) -> None:
+        """
+        Tests that GithubOrgClient.org returns the correct value.
+
+        Args:
+            org_name (str): The name of the GitHub organization to test.
+            mock_get_json (Mock): The mocked get_json function
+        """
+        mock_get_json.return_value = {"name": org_name}
+        client = GithubOrgClient(org_name)
+        result = client.org
+
+        # verify that the get_json was called once with the expected URL
+        mock_get_json.assert_called_once_with(
+            f'https://api.github.com/orgs/{org_name}'
+            )
+
+        self.assertEqual(result, {"name": org_name})
 
 
 if __name__ == "__main__":
